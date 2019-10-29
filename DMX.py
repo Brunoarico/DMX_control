0e139122122a3f1da445caf837dcfe19e4d10ef9
Brunoarico <brunoarico@gmail.com>
2019-10-29 18:53:26 -0300

    working with no bugs

--- DMX.py
+++ DMX.py
@@ -1,11 +1,31 @@
 import time
 import dmx
 
-sender = dmx.DMX_Serial()
-sender.start()
-for i in range(200):
-    if i % 2:
-        sender.set_data(bytes((255,)*512))
-    else:
-        sender.set_data(bytes((0,)*512))
-    time.sleep(1)
+class pyDMX:
+    def __init__(self, devName = "/dev/ttyUSB0"):
+        self.arrayBytes = [0]*512
+        self.sender = dmx.DMX_Serial(devName)
+
+    def set_channel(self, ch, val):
+        self.arrayBytes[ch-1] = val
+        print(bytearray(self.arrayBytes))
+        self.sender.set_data(bytearray(self.arrayBytes))
+    def run(self):
+        print("starting...")
+        self.sender.start()
+
+    def halt(self):
+        self.sender.stop()
+
+dmx = pyDMX("/dev/ttyUSB2")
+dmx.run()
+dmx.set_channel(1, 255)
+while(True):
+    dmx.set_channel(2, 255)
+    dmx.set_channel(3, 0)
+    time.sleep(0.5)
+    dmx.set_channel(2, 0)
+    dmx.set_channel(3, 255)
+    time.sleep(0.5)
+
+dmx.halt()
