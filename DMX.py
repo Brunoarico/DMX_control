97210faeaf510b87c602aabe421b430704166d5f
Brunoarico <brunoarico@gmail.com>
2019-10-29 18:50:36 -0300

    Upgrade

--- DMX.py
+++ DMX.py
@@ -1,2 +1,11 @@
 import time
-import serial
+import dmx
+
+sender = dmx.DMX_Serial()
+sender.start()
+for i in range(200):
+    if i % 2:
+        sender.set_data(bytes((255,)*512))
+    else:
+        sender.set_data(bytes((0,)*512))
+    time.sleep(1)
